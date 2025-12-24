6201778d63cf302689845550fa7d87a68b84ae89
Aditya Moorjmalani <adityamoorjmalani@Adityas-MacBook-Air.local>
2025-12-25 02:50:35 +0530

    Created more routes for projects,tasks, teams, tags etc

--- index.js
+++ index.js
@@ -137,6 +137,270 @@ app.get("/auth/me", authenticateToken, async (req, res) => {
   }
 });
 
+const createTask = async (newTask) => {
+  try {
+    const task = new Task(newTask);
+    const saveTask = await task.save();
+    return saveTask;
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.post("/tasks", async (req, res) => {
+  try {
+    const savedTask = await createTask(req.body);
+    if (!savedTask)
+      return res.status(400).json({ error: "Failed to create Task" });
+
+    res.status(201).json({ message: "Task created successfully", savedTask });
+  } catch (error) {
+    console.error("Error creating task", error);
+    res.status(500).json({ error: "Internal Server error." });
+  }
+});
+
+const readAllTasks = async (filters = {}) => {
+  try {
+    const query = {};
+
+    if (filters.team) query.team = filters.team;
+    if (filters.owner) query.owners = { $in: filters.owner.split(",") };
+    if (filters.Project) query.Project = filters.Project;
+    if (filters.status) query.status = filters.status;
+    if (filters.tags) query.tags = { $in: filters.tags.split(",") };
+
+    const tasks = await Task.find(query);
+    return tasks;
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.get("/tasks", async (req, res) => {
+  try {
+    const filters = {
+      team: req.query.team,
+      owner: req.query.owner,
+      project: req.query.project,
+      status: req.query.status,
+      tags: req.query.tags,
+    };
+
+    const tasks = await readAllTasks(filters);
+
+    if (tasks.length === 0) {
+      return res.status(404).json({ error: "No tasks found." });
+    }
+
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.error("Error fetching tasks", error);
+    res.status(500).json({ error: "Failed to fetch tasks" });
+  }
+});
+
+const updateTask = async (taskId, updatedData) => {
+  try {
+    const taskUpdate = await Task.findByIdAndUpdate(taskId, updatedData, {
+      new: true,
+    });
+    return taskUpdate;
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.put("/tasks/:id", async (req, res) => {
+  try {
+    const updatedTask = await updateTask(req.params.id, req.params.body);
+
+    if (!updateTask) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    res.status(200).json({ message: "Task updated successfully", updateTask });
+  } catch (error) {
+    console.error("Error in updating task", error);
+    res.status(500).json({ error: "Failed to update task" });
+  }
+});
+
+const deleteTask = async (taskId) => {
+  try {
+    const deletedTask = await Task.findByIdAndDelete(taskId);
+    return deleteTask;
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const deletedTask = await deleteTask(req.params.id);
+
+    if (!deletedTask) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    res.status(200).json({ message: "Task deleted successfully", deletedTask });
+  } catch (error) {
+    console.error("Error in deleting task.");
+    res.status(500).json({ error: "Failed to delete task." });
+  }
+});
+
+const createTeam = async (newTeam) => {
+  try {
+    const team = new Team(newTeam);
+    const saveTeam = await team.save();
+    return saveTeam;
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.post("/teams", async (req, res) => {
+  try {
+    const savedTeam = await createTeam(req.body);
+
+    if (!savedTeam) {
+      return res.status(400).json({ error: "Failed to create new Team." });
+    }
+
+    res.status(201).json(savedTeam);
+  } catch (error) {
+    console.error("Error in creating new Team.");
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+const readAllTeams = async () => {
+  try {
+    const teams = await Team.find();
+    return teams;
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.get("/teams", async (req, res) => {
+  try {
+    const teams = await readAllTeams();
+
+    if (teams.length === 0) {
+      return res.status(404).json({ error: "No teams found." });
+    }
+
+    res.status(200).json(teams);
+  } catch (error) {
+    console.error("Error in fetching teams");
+    res.status(500).json({ error: "Failed to fetch teams" });
+  }
+});
+
+const createProject = async (newProject) => {
+  try {
+    const project = new Project(newProject);
+    const saveProject = await project.save();
+    return saveProject;
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.post("/projects", async (req, res) => {
+  try {
+    const savedProject = await createProject(req.body);
+
+    if (!savedProject) {
+      return res.status(400).json({ error: "Failed to create project." });
+    }
+
+    res
+      .status(201)
+      .json({ message: "Project created successfully.", savedProject });
+  } catch (error) {
+    console.error("Error in creating project.");
+    res.status(500).json({ error: "Internal Server Error." });
+  }
+});
+
+const readAllProjects = async () => {
+  try {
+    const projects = await Project.find();
+    return projects;
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.get("/projects", async (req, res) => {
+  try {
+    const projects = await readAllProjects();
+
+    if (projects.length === 0) {
+      return res.status(404).json({ error: "Projects not found." });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Projects fetched successfully", projects });
+  } catch (error) {
+    console.error("Error in fetching projects");
+    res.status(500).json({ error: "Failed to fetch projects." });
+  }
+});
+
+const createTag = async (newTag) => {
+  try {
+    const tag = new Tag(newTag);
+    const saveTag = await tag.save();
+    return saveTag;
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.post("/tags", async (req, res) => {
+  try {
+    const savedTag = await createTag(req.body);
+
+    if (!savedTag) {
+      return res.status(400).json({ error: "Failed to create tag." });
+    }
+
+    res.status(201).json({ message: "Tag created successfully.", savedTag });
+  } catch (error) {
+    console.error("Error in creating tag.");
+    res.status(500).json({ error: "Internal server error." });
+  }
+});
+
+const readAllTags = async () => {
+  try {
+    const tags = await Tag.find();
+    return tags;
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.get("/tags", async (req, res) => {
+  try {
+    const tags = await readAllTags();
+
+    if (tags.length === 0) {
+      return res.status(404).json({ error: "Tags Not found." });
+    }
+
+    res.status(200).json({ tags });
+  } catch (error) {
+    console.error("Error in fetching tags.");
+    res.status(500).json({ error: "Failed to fetch tags" });
+  }
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
