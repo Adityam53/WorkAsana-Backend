4cd59444bd386c8f74643b16af7e6a11bcc1bfca
Aditya Moorjmalani <adityamoorjmalani@Adityas-MacBook-Air.local>
2026-01-04 19:27:47 +0530

    misc

--- index.js
+++ index.js
@@ -499,7 +499,7 @@ app.get("/report/closed-tasks", async (req, res) => {
   }
 });
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log("Server is running on port", PORT);
-// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
